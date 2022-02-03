944793f45277ef48233b43d4ebadb33f04917f5a
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-02-03 11:43:02 -0600

    added Base Init

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Base Model """
+
+class BaseModel:
+    """ Base class all other classes will inherit from """
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize the base model """
+        pass
