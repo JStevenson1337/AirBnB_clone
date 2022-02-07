1eace772d73a2aac3c7a84ba3fe91b5f4ac04546
jstevenon1337 <jeremy.m.stevenson@gmail.com>
2022-02-07 06:25:31 -0600

    changed init function

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
 
 # !/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
+""" init file for models package """
 from models.engine.file_storage import FileStorage
 
-"""A variable storage, an instance of FileStorage"""
+""" define storage """
 storage = FileStorage()
 storage.reload()
+
